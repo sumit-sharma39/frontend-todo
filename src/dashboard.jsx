f59df29e3d31e9134e02783c1026906e9ee087a2
Lichtリヒト <sharmarahul1729@gmail.com>
2026-03-01 14:29:02 +0530

    fixed dark/light mode

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -1,20 +1,27 @@
 import { Link } from "react-router-dom";
-import "./main.css"
-export function Dashboard() {
-    return(
+import "./main.css";
+
+export function Dashboard({ darkMode, setDarkMode }) {
+    return (
         <div className="Dashboard">
-            <h1>To-do list</h1>
+        <h1>To-do list</h1>
 
-            <div className="dashboard-buttons">
-                
-                <Link to="/Add" className="edit-btn link-add">
-                    Add
-                </Link>
-                <Link to="/" className="edit-btn link-add">
-                    logout
-                </Link>
-            </div>
+        <div className="dashboard-buttons">
+            <Link to="/Add" className="edit-btn link-add">
+            Add
+            </Link>
 
+            <Link to="/" className="edit-btn link-add">
+            Logout
+            </Link>
+
+            <button
+            className="edit-btn"
+            onClick={() => setDarkMode(!darkMode)}
+            >
+            {darkMode ? "Light Mode" : "Dark Mode"}
+            </button>
         </div>
-    )
+        </div>
+    );
 }
--- src/resetpassword.jsx
+++ src/resetpassword.jsx
@@ -19,7 +19,7 @@ export function ResetPassword() {
 
         try {
         const res = await axios.post(
-            `http://localhost:5000/reset-password/${token}`,
+            `https://backend-todo-1-z9rj.onrender.com/reset-password/${token}`,
             { password }
         );
 
@@ -27,7 +27,7 @@ export function ResetPassword() {
 
         // Redirect to login after 2 seconds
         setTimeout(() => {
-            navigate("/");
+            navigate("/login");
         }, 2000);
 
         } catch (err) {
