bfe76c6e89c1ac8642b6f0ec90bc1b38858d2409
Lichtリヒト <sharmarahul1729@gmail.com>
2026-03-06 20:21:08 +0530

    Add cookies destroy

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -1,19 +1,34 @@
-import { Link } from "react-router-dom";
-import "./main.css";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
 
 export function Dashboard() {
+
+    const navigate = useNavigate();
+
+    const logout = async () => {
+        await axios.post(
+        "https://backend-todo-1-z9rj.onrender.com/logout",
+        {},
+        { withCredentials: true }
+        );
+
+        navigate("/");
+    };
+
     return (
         <div className="Dashboard">
         <h1>To-do list</h1>
 
         <div className="dashboard-buttons">
+
             <Link to="/Add" className="edit-btn link-add">
             Add
             </Link>
 
-            <Link to="/" className="edit-btn link-add">
+            <button onClick={logout} className="edit-btn link-add">
             Logout
-            </Link>
+            </button>
+
         </div>
         </div>
     );
